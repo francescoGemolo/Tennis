d0bc59c0a4a7c6c7bcbe0caa64561acbb1b34494
Francesco <francescolavoro0209@gmail.com>
2026-07-24 15:51:42 +0200

    PWA & UI Bugfix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: false,
+      includeAssets: ['icons/apple-touch-icon.png'],
+      manifest: {
+        name: 'Tennis Salandra',
+        short_name: 'Tennis Salandra',
+        description: 'Prenota il tuo turno al Circolo Tennis in pochi tocchi.',
+        lang: 'it',
+        start_url: '/Tennis/',
+        scope: '/Tennis/',
+        display: 'standalone',
+        background_color: '#0E1116',
+        theme_color: '#0E1116',
+        icons: [
+          { src: 'icons/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icons/icon-512.png', sizes: '512x512', type: 'image/png' },
+        ],
+      },
+    }),
+  ],
   base: '/Tennis/',
   build: {
     chunkSizeWarningLimit: 1000
