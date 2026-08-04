7439eeca005f2d3a66f393a338bb25c43c206a5e
Francesco <francescolavoro0209@gmail.com>
2026-08-04 15:18:27 +0200

    Tailwind CSS Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 const BASE = '/'
@@ -8,6 +9,7 @@ const BASE = '/'
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
       injectRegister: false,
